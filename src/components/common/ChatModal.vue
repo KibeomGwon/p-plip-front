5e3ca9854b54a48e2d0bd54be1b265c991183047
smj1513 <minj21@naver.com>
2025-12-06 22:36:49 +0900

    feat: 플랜 화면 추가 및 AI 아이콘 변경, 메인화면 드래그바 수정

--- src/components/common/ChatModal.vue
+++ src/components/common/ChatModal.vue
@@ -67,7 +67,11 @@
     <!-- Chat Trigger Button -->
     <button class="chat-fab" @click="isOpen = true" :class="{ hidden: isOpen }">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M21 11.5C21.0034 12.8199 20.6951 14.1219 20.1 15.3C19.3944 16.7118 18.3098 17.8992 16.9674 18.7293C15.6251 19.5594 14.0782 19.9994 12.5 20C11.1801 20.0035 9.87812 19.6951 8.7 19.1L3 21L4.9 15.3C4.30493 14.1219 3.99656 12.8199 4 11.5C4.00061 9.92179 4.44061 8.37488 5.27072 7.03258C6.10083 5.69028 7.28825 4.6056 8.7 3.90003C9.87812 3.30496 11.1801 2.99659 12.5 3.00003H13C15.0843 3.11502 17.053 3.99479 18.5291 5.47089C20.0052 6.94699 20.885 8.91568 21 11V11.5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12 5C13.5 9 15 10.5 19 12C15 13.5 13.5 15 12 19C10.5 15 9 13.5 5 12C9 10.5 10.5 9 12 5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        
+        <path d="M19 2C19.5 3.5 20.5 4.5 22 5C20.5 5.5 19.5 6.5 19 8C18.5 6.5 17.5 5.5 16 5C17.5 4.5 18.5 3.5 19 2Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        
+        <path d="M5 16C5.5 17.5 6.5 18.5 8 19C6.5 19.5 5.5 20.5 5 22C4.5 20.5 3.5 19.5 2 19C3.5 18.5 4.5 17.5 5 16Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </button>
   </div>
