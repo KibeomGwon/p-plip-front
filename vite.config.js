daf3fab3735834146f11602a46e6a3707e4616dd
KibeomGwon <xtcc02332@naver.com>
2025-12-07 16:20:50 +0900

    Feat: 로그인페이지, 회원가입 페이지, 프로필 수정 페이지 완료 메인 페이지 목록이 화면 하단에서 4분의 1 지점 이하일 때 사라지게 설정완료

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,40 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PPLIP',
+        short_name: 'PPLIP',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/pplip.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pplip.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   server: {
     // 모든 외부 호스트 접속 허용
